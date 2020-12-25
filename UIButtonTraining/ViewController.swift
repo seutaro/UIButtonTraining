4723018ed385f04b23caae74985a9ecf7fd452fa
seutaro <seutaros@gmail.com>
2020-12-25 16:11:34 +0900

    動くボタンを実装

--- UIButtonTraining/ViewController.swift
+++ UIButtonTraining/ViewController.swift
@@ -14,6 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonTapped(_ sender: UIButton) {
+        animateView(sender)
+    }
+    
+    
+    func animateView(_ viewToAnimate:UIView) {
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut, animations: {
+            viewToAnimate.transform = CGAffineTransform(scaleX: 1.08, y: 1.08)
+        }) { (_) in
+            UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
+                viewToAnimate.transform = .identity
+                
+            }, completion: nil)
+        }
+    }
+    
+    
+    
+    
+    
 }
-

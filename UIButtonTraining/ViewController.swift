2182fe3e5f0cf5efb7db57325a183b0e05e74281
seutaro <seutaros@gmail.com>
2020-12-25 15:22:31 +0900

    initial commit

---
+++ UIButtonTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIButtonTraining
+//
+//  Created by shotaro takahashi on 2020/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
